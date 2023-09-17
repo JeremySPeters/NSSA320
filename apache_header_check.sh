9e648ea51fecb1e8c01eae4dc57daa1e0d2dae3f
JeremySPeters <73489096+JeremySPeters@users.noreply.github.com>
2023-09-16 22:05:38 -0400

    Update apache_header_check.sh

--- apache_header_check.sh
+++ apache_header_check.sh
@@ -2,17 +2,17 @@
 
 # Check if an argument is provided
 if [ "$#" -ne 1 ]; then
-    echo '{"error": "Usage: '$0' <ip_address_or_domain>"}'
+    echo "{\"error\": \"Usage: $0 <ip_address_or_domain>\"}"
     exit 1
 fi
 
 # Fetch the server header
-server_header=$(curl -sI http://$1 | grep "Server:")
+server_header=$(curl -sI "http://$1" | grep "Server:")
 
 # Check and display the server header in JSON format
 if [ -z "$server_header" ]; then
-    echo "{\"server_header_found\": false}"
+    echo '{"server_header_found": false}'
 else
     header_value=$(echo "$server_header" | sed 's/Server: //')
-    echo "{\"server_header_found\": true, \"header_value\": \"$header_value\"}"
+    echo "{\"server_header_found\": true, \"header_value\": \"${header_value}\"}"
 fi
