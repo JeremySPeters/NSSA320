52de795aa314aa9ece59badc3bdb7a351b64e257
JeremySPeters <73489096+JeremySPeters@users.noreply.github.com>
2023-09-16 23:34:59 -0400

    Update root_directory_check.sh

--- root_directory_check.sh
+++ root_directory_check.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Check if an argument is provided
-if [ "$#" -ne 1 ]; then
-    echo '{"error": "Usage: '$0' <ip_address_or_domain>"}'
-    exit 1
-fi
+# Locations for the Apache config (change these if your setup is different)
+CONFIG_FILES=("/etc/apache2/apache2.conf" "/etc/httpd/httpd.conf")
 
-# Get the HTTP response code
-http_response=$(curl -o /dev/null -s -w "%{http_code}" "http://$1/")
+DIRECTORY_LISTING_DISABLED=true
 
-# Check the HTTP response code
-if [[ $http_response -eq 403 ]]; then
+# Check each config file for the "Indexes" option
+for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
+    if [ -f "$CONFIG_FILE" ]; then
+        # If "Indexes" is found without a preceding "-", set DIRECTORY_LISTING_DISABLED to false
+        if grep -E '^[[:space:]]*Options[[:space:]]+[^#]*\bIndexes\b' "$CONFIG_FILE" > /dev/null; then
+            DIRECTORY_LISTING_DISABLED=false
+            break
+        fi
+    fi
+done
+
+# Output the result in JSON format
+if $DIRECTORY_LISTING_DISABLED; then
     echo '{"directory_listing_disabled": true}'
 else
     echo '{"directory_listing_disabled": false}'
